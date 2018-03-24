f0a84490e13f50d0dc69569dc01e73059d607337
hawkup <spiritsquare@gmail.com>
2018-03-24 18:29:38 +0700

    Overlay preview screenshot on multitask switcher (iOS) and Added Readme

--- ios/reactnativepreventscreenshot/AppDelegate.m
+++ ios/reactnativepreventscreenshot/AppDelegate.m
@@ -34,4 +34,34 @@
   return YES;
 }
 
+- (void)applicationWillResignActive:(UIApplication *)application {
+  
+  // fill screen with our own colour
+  UIView *colourView = [[UIView alloc]initWithFrame:self.window.frame];
+  colourView.backgroundColor = [UIColor whiteColor];
+  colourView.tag = 1234;
+  colourView.alpha = 0;
+  [self.window addSubview:colourView];
+  [self.window bringSubviewToFront:colourView];
+  
+  // fade in the view
+  [UIView animateWithDuration:0.5 animations:^{
+    colourView.alpha = 1;
+  }];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+  
+  // grab a reference to our coloured view
+  UIView *colourView = [self.window viewWithTag:1234];
+  
+  // fade away colour view from main view
+  [UIView animateWithDuration:0.5 animations:^{
+    colourView.alpha = 0;
+  } completion:^(BOOL finished) {
+    // remove when finished fading
+    [colourView removeFromSuperview];
+  }];
+}
+
 @end
