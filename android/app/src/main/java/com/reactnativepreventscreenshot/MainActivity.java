9f1dba4437f621c54538be55a5aa9e5e3f351af3
hawkup <spiritsquare@gmail.com>
2018-03-24 17:55:33 +0700

    Added set secure flag for prevent screenshot on android

--- android/app/src/main/java/com/reactnativepreventscreenshot/MainActivity.java
+++ android/app/src/main/java/com/reactnativepreventscreenshot/MainActivity.java
@@ -18,5 +18,6 @@ public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
     }
 }
